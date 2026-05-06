c8c96e82013d751d86e8e0e836eb318edb256396
cashew-olddew <cashew-olddew@users.noreply.github.com>
2026-05-06 16:48:07 +0000

    deploy: 6ac2d5c56f9ecdded28045caaa9097de9f560a94

---
+++ assets/hello-world copy 2-Bi5E34yG.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-RygUoThq.js";var t=e();function n(e){return(0,t.jsx)({h1:`h1`,...e.components}.h1,{children:`Hey 3! 🥜`})}function r(e={}){let{wrapper:r}=e.components||{};return r?(0,t.jsx)(r,{...e,children:(0,t.jsx)(n,{...e})}):n(e)}export{r as default};
--- assets/hello-world copy 2-pdNhtWDl.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./index-B0-txgUd.js";var t=e();function n(e){return(0,t.jsx)({h1:`h1`,...e.components}.h1,{children:`Hey 3! 🥜`})}function r(e={}){let{wrapper:r}=e.components||{};return r?(0,t.jsx)(r,{...e,children:(0,t.jsx)(n,{...e})}):n(e)}export{r as default};
