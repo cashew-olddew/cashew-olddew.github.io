ab9bc347512afec364d7c0aedb68d6b97ce44cee
cashew-olddew <cashew-olddew@users.noreply.github.com>
2026-05-15 22:14:36 +0000

    deploy: a67601393cbd9fdfd48b75121e4b569e2b6f2024

---
+++ assets/make-it-exist-first-CGzKi0CU.js
@@ -0,0 +1,57 @@
+import{a as e}from"./index-CkJIoNiL.js";import{i as t,n,r}from"./components-Djiu-NHM.js";var i=e(),a=`/assets/meme-DYwAtQTI.png`,o=`/assets/meme2-Cy6jVvAs.png`;function s(e){let s={a:`a`,blockquote:`blockquote`,em:`em`,h1:`h1`,h4:`h4`,li:`li`,p:`p`,strong:`strong`,ul:`ul`,...e.components};return(0,i.jsxs)(i.Fragment,{children:[(0,i.jsx)(s.h1,{children:`Make it Exist First`}),`
+`,(0,i.jsx)(s.p,{children:`There's a good chance you have some sort of ⭐ideas⭐ folder, paper, journal, or that, maybe, you keep them all in your head.
+Whatever the case, among those brilliant ideas, there are probably some that you haven't touched in months or years.`}),`
+`,(0,i.jsx)(n,{align:`center`,children:(0,i.jsx)(s.p,{children:(0,i.jsx)(s.em,{children:`Why haven't you touched them?`})})}),`
+`,(0,i.jsxs)(s.p,{children:[`Well, maybe those ideas are `,(0,i.jsx)(s.em,{children:`"not good enough"`}),`, or maybe they're `,(0,i.jsx)(s.em,{children:`"too big"`}),` to get started with them.
+Maybe you already started working on them, but you just can't get them to that sweet spot in which you're finally happy to show them to the world.`]}),`
+`,(0,i.jsxs)(s.p,{children:[`"Just a little more work until they're `,(0,i.jsx)(s.em,{children:`perfect`}),`."`]}),`
+`,(0,i.jsx)(t,{label:`Image Credits: growthbyvisuals`,width:360,children:(0,i.jsx)(`img`,{src:a,alt:`Make it Exist First`})}),`
+`,(0,i.jsxs)(s.p,{children:[`I've been finding myself in this situation for some time. My `,(0,i.jsx)(s.a,{href:`https://www.youtube.com/@cashewolddew`,children:`youtube channel`}),` is an especially obvious case.
+I have a folder with over 50 video ideas, partial scripts, even fully completed scripts. Yet, they've somehow never seen the light of day.`]}),`
+`,(0,i.jsx)(s.p,{children:(0,i.jsx)(s.em,{children:`Why?`})}),`
+`,(0,i.jsxs)(s.p,{children:[(0,i.jsx)(s.em,{children:`In my mind`}),` it is because those videos need to, at least, respect a minimum quality standard.
+I do not expect them to be perfect, but I am aware that I would waste my watchers' time if I were to upload an unedited, long, and not so well thought video.
+Add the friction of thumbnails, descriptions, tags, links, documentation, and no wonder things are the way they are.`]}),`
+`,(0,i.jsx)(r,{}),`
+`,(0,i.jsx)(s.p,{children:`Alright, but that just means that I should pick, at least some of my ideas to turn into reality, accept that others are doomed to fail, and move on...`}),`
+`,(0,i.jsxs)(s.blockquote,{children:[`
+`,(0,i.jsx)(s.p,{children:`So long, video idea in which I discuss the weird particularities of shaders applied on Godot labels, you had a good run!`}),`
+`,(0,i.jsxs)(s.p,{children:[`Yes, some game developers might be wasting hours of their lives trying to find a solution to those quirks, but hey, at least you made that `,(0,i.jsx)(s.em,{children:`perfect`}),` video 2 months ago!`]}),`
+`]}),`
+`,(0,i.jsx)(s.p,{children:`Do you see the problem? How many ideas have to die because of the friction or because they're not perfect?`}),`
+`,(0,i.jsx)(s.p,{children:`Well, maybe not all ideas have to be perfect..`}),`
+`,(0,i.jsx)(t,{label:`Image Credits: growthbyvisuals`,width:360,children:(0,i.jsx)(`img`,{src:o,alt:`Ugly action beats unfinished perfection`})}),`
+`,(0,i.jsx)(n,{align:`center`,children:(0,i.jsx)(s.p,{children:(0,i.jsx)(s.strong,{children:`Just get things out there!`})})}),`
+`,(0,i.jsxs)(s.p,{children:[`An idea that reaches 10 people is way more valuable than one that reaches nobody. Zero is not "almost something", it's `,(0,i.jsx)(s.strong,{children:`nothing`}),`.
+When giving up on ideas, you are not compromising quality, you're chosing existance over non-existance.`]}),`
+`,(0,i.jsxs)(s.p,{children:[`You're losing the benefit of getting feedback from others. You're losing the time spent on building or thinking about that idea.
+Keeping the idea well tucked in its folder only rewards you with `,(0,i.jsx)(s.em,{children:`what ifs`}),` and more self-doubts created by another failed project.`]}),`
+`,(0,i.jsx)(s.p,{children:`Don't do this! Make is visible! ⭐Your ideas⭐ are worth sharing!`}),`
+`,(0,i.jsx)(r,{}),`
+`,(0,i.jsx)(s.p,{children:`Take this blog for example. I'm not sure of the site's state by the time you read this, but I've been wanting to make an OS-like blog for years.
+However, setting up a blog is no easy task, especially considering the OS twist. I just couldn't get started.
+This was doomed to be a project, forever to be pushed back by other more worthy, or smaller projects.`}),`
+`,(0,i.jsx)(s.p,{children:(0,i.jsx)(s.em,{children:`Why?`})}),`
+`,(0,i.jsxs)(s.p,{children:[`Because in most places you look, you see `,(0,i.jsx)(s.em,{children:`perfect`}),` sites, wonderfully written texts, complexity. Finishing and polishing such a project from scratch could take up months.`]}),`
+`,(0,i.jsxs)(s.p,{children:[`So... why finish it? Why not just put it out there? It doesn't have to be perfect, it doesn't need to immediately work for all devices, it doesn't need extraordinary styling.
+It needs to serve a purpose, and I think that for a blog post called 'Make it exist first', it's actually `,(0,i.jsx)(s.em,{children:`perfect`}),` despite being `,(0,i.jsx)(s.em,{children:`imperfect`}),`.`]}),`
+`,(0,i.jsx)(r,{}),`
+`,(0,i.jsx)(n,{align:`center`,children:(0,i.jsx)(s.p,{children:(0,i.jsx)(s.strong,{children:`Now.. what's waiting in that folder of yours?`})})}),`
+`,(0,i.jsx)(r,{}),`
+`,(0,i.jsx)(s.h4,{children:`Practical footnote`}),`
+`,(0,i.jsx)(s.p,{children:`If you're interested in Game Development or any thoughts like this one, here's where to find me (Also, I wasn't kidding, the site really is a work in progress, so I don't have icons for socials yet!):`}),`
+`,(0,i.jsxs)(s.ul,{children:[`
+`,(0,i.jsxs)(s.li,{children:[(0,i.jsx)(s.a,{href:`https://www.youtube.com/@cashewolddew`,children:`Youtube`}),`📺`]}),`
+`,(0,i.jsxs)(s.li,{children:[(0,i.jsx)(s.a,{href:`https://github.com/cashew-olddew/`,children:`Github`}),`🐙🐈‍⬛`]}),`
+`,(0,i.jsxs)(s.li,{children:[(0,i.jsx)(s.a,{href:`https://discord.gg/3jkTF9FgWJ`,children:`Discord`}),`💬`]}),`
+`,(0,i.jsxs)(s.li,{children:[(0,i.jsx)(s.a,{href:`https://bsky.app/profile/cashewolddew.bsky.social`,children:`Bluesky`}),`🦋`]}),`
+`,(0,i.jsxs)(s.li,{children:[(0,i.jsx)(s.a,{href:`https://x.com/CashewOldDew`,children:`X / Twitter`}),`✖️🐤`]}),`
+`]}),`
+`,(0,i.jsx)(s.p,{children:`Making content takes time, and your support helps me make more of it:`}),`
+`,(0,i.jsxs)(s.ul,{children:[`
+`,(0,i.jsxs)(s.li,{children:[(0,i.jsx)(s.a,{href:`https://ko-fi.com/cashewolddew`,children:`Ko-Fi`}),`☕`]}),`
+`,(0,i.jsxs)(s.li,{children:[(0,i.jsx)(s.a,{href:`https://www.patreon.com/c/CashewOldDew`,children:`Patreon`}),` P`]}),`
+`,(0,i.jsxs)(s.li,{children:[(0,i.jsx)(s.a,{href:`https://www.youtube.com/channel/UC4Tv4nxn8yZjyZKMvs_NvAg/join`,children:`Youtube Membership`}),` 📺⭐`]}),`
+`,(0,i.jsxs)(s.li,{children:[(0,i.jsx)(s.a,{href:`https://store.steampowered.com/app/3032430/Fangs__Faith_Solitaire/`,children:`Buy Fangs & Faith Solitaire`}),` 🧛`]}),`
+`]}),`
+`,(0,i.jsx)(r,{})]})}function c(e={}){let{wrapper:t}=e.components||{};return t?(0,i.jsx)(t,{...e,children:(0,i.jsx)(s,{...e})}):s(e)}export{c as default};
