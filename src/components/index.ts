97465229207a3abd0121c51cd56de1def3512258
cashew-olddew <cashewolddew@gmail.com>
2026-05-20 17:33:58 +0300

    reveal section

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { Media, MediaGroup } from './media/media'
 export { Separator, Align } from './prose/prose-components'
+export { RevealSection } from './prose/reveal-section'
 export { ShaderPreview } from './shader-preview/shader-preview'
 export { SupportMe } from './support-me/support-me'
